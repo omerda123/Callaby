83de2034dfb212506a20a2f86ad6e887df1f3be1
Omer Daniel <omerda@gmail.com>
2020-02-10 14:00:18 +0200

    add models V1

--- src/chat/models.py
+++ src/chat/models.py
@@ -1,6 +1,34 @@
 from django.db import models
 
+
 class Message(models.Model):
     author = models.CharField(max_length=200)
     content = models.TextField()
-    timestamp = models.TimeField(auto_now=True) 
+    timestamp = models.TimeField(auto_now=True)
+    chat_id = models.CharField(max_length=200)
+
+
+class Roles(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Enterprise(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class AdminUser(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    role = models.ForeignKey(Roles, on_delete=models.CASCADE)
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+
+
+class Agent(models.Model):
+    enterprise = models.ForeignKey(Enterprise, on_delete=models.CASCADE)
+    private_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+class Chat(models.Models):
+    originator = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+    chat_id = = models.CharField(max_length=200)
